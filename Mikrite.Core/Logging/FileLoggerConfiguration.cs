37d64d9080a8faa7ddcbf6340dccf87b5d98c7a4
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-07-30 01:35:20 +0200

    Add project files.

---
+++ Mikrite.Core/Logging/FileLoggerConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Mikrite.Core.Logging
+{
+    /// <summary>
+    /// The main configuration for the <seealso cref="FileLogger"/>.
+    /// </summary>
+    public class FileLoggerConfiguration
+    {
+        /// <summary>
+        /// The minimal log level of the <seealso cref="FileLogger"/>.
+        /// </summary>
+        public LogLevel LogLevel { get; set; } = LogLevel.Trace;
+
+        /// <summary>
+        /// The boolean state if the time of the log should be logged.
+        /// </summary>
+        public bool LogTime { get; set; } = true;
+
+        /// <summary>
+        /// The boolean state if the log level should be logged.
+        /// </summary>
+        public bool OutputLogLevel { get; set; } = true;
+    }
+}
